8313bc98af6c65c10a91183b90518e822fcf6ee3
luy <143974574+l4uy@users.noreply.github.com>
2026-02-15 18:48:08 -0600

    fix 2

--- setup.sh
+++ setup.sh
@@ -183,23 +183,6 @@ sudo tee /etc/caddy/Caddyfile <<EOF
         on_demand
     }
 
-    handle_path /yay/* {
-        reverse_proxy h ttps://openairtowhardworking.com {
-            header_up Host openairtowhardworking.com
-            header_up X-Real-IP {remote_host}
-            header_up X-Forwarded-For {remote_host}
-            header_up X-Forwarded-Proto {scheme}
-        }
-    }
-
-    handle_path /analytics/* {
-        reverse_proxy https://www.googletagmanager.com {
-            header_up Host www.googletagmanager.com
-            header_up X-Real-IP {remote_host}
-            header_up X-Forwarded-For {remote_host}
-        }
-    }
-
     @websockets {
         path /w/*
         header Connection *Upgrade*
@@ -254,7 +237,14 @@ sudo tee /etc/caddy/Caddyfile <<EOF
         }
     }
     
-    handle_path /api/* {
+    handle /api/auth/* {
+        reverse_proxy 127.0.0.1:5000 {
+             header_up X-Real-IP {remote_host}
+             header_up X-Forwarded-For {remote_host}
+        }
+    }
+
+    handle /api/sync/* {
         reverse_proxy 127.0.0.1:5000 {
              header_up X-Real-IP {remote_host}
              header_up X-Forwarded-For {remote_host}
